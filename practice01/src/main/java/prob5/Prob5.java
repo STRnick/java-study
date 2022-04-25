7cf176f1719fae1b6d1fe7c04ddac1db9d3bef06
lsm <strnick@gmail.com>
2022-04-25 18:05:15 +0900

    Prob5 Solved

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,5 +3,17 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
+		
+		int i;
+		int num = 0;
+		
+		for(i=1;i<=99;i++) {
+			if(i == 3 | i == 6 | i == 9) {
+				System.out.println(i + " 짝");
+			}
+			else if(i % 10 == 3 | i % 10 == 6 || i % 10 == 9) {
+				System.out.println(i + " 짝");
+			}
+		}
 	}
 }
