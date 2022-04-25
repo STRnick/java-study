4a32d83d778fba85389be1b71820bb28d0f04937
lsm <strnick@gmail.com>
2022-04-25 17:31:10 +0900

    Prob3 Solved

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -9,9 +9,28 @@ public class Prob3 {
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
-		
-		
-		
+		for(int i=0;i<=2;i++) {
+			System.out.print("숫자를 입력하세요 : ");
+			int number = scanner.nextInt();
+			int Even_sum = 0 ;
+			int Odd_sum = 0;
+			
+			if(number % 2 == 0) {
+				for(int j=2;j<=number;j+=2) {
+					Even_sum += j;
+				}
+				System.out.println("결과 값 : " + Even_sum);
+			}
+			else if(number % 2 != 0) {
+				for(int j=1;j<=number;j+=2) {
+					Odd_sum += j;
+				}
+				System.out.println("결과 값 : " + Odd_sum);
+			}
+			else {
+				System.out.println("입력값 오류!!");
+			}
+		}
 		scanner.close();
 	}
 }
