34e6c7bbd0289fd9765a78ed96ba12821ae7af3b
lsm <nicksumin97@gmail.com>
2022-04-26 19:42:16 +0900

    Prob01 Solved

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -9,8 +9,15 @@ public class Prob01 {
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 
 		/* 코드 작성 */
+		System.out.print("금액 : ");
+		int price = scanner.nextInt();
 		
-		
+		for(int i = 0; i < MONEYS.length; i++) {
+			int cal = price / MONEYS[i];
+			price -= (cal * MONEYS[i]);
+			System.out.println(MONEYS[i] + "원 : " + cal + "개");
+		}
+				
 		scanner.close();
  	}
 }
