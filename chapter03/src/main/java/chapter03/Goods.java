82c99eac85046d4610d48750353c9ec6f14c23b7
lsm <strnick@gmail.com>
2022-04-27 14:43:38 +0900

    class practice

--- chapter03/src/main/java/chapter03/Goods.java
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -1,10 +1,15 @@
 package chapter03;
 
 public class Goods {
+	public static int countOfGoods;
 	private String name;
 	private int price;
 	private int countStock;
 	private int countSold;
+
+	public Goods() {
+		Goods.countOfGoods = countOfGoods + 1;
+	}
 
 	public String getName() {
 		return name;
@@ -40,6 +45,15 @@ public class Goods {
 
 	public void setCountSold(int countSold) {
 		this.countSold = countSold;
+	}
+
+	public void showInfo() {
+		System.out.println("name : " + name + ", price : " + price + ", countStock : " + countStock + ", countSold : "
+				+ countSold);
+	}
+
+	public int calcDiscountPrice(double discountrate) {
+		return (int)(discountrate * price);
 	}
 
 }
