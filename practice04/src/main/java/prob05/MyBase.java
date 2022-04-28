4289ad107e840604c7f566fe839414ae26457bdb
STRnick <nicksumin97@naver.com>
2022-04-28 18:31:00 +0900

    강의장 자료 동기화

--- practice04/src/main/java/prob05/MyBase.java
+++ practice04/src/main/java/prob05/MyBase.java
@@ -1,7 +1,7 @@
 package prob05;
 
 public class MyBase extends Base {
-	public class Base {
+	public class Base extends MyBase{
 		public void service(String state) {
 			if (state.equals("낮")) {
 				day();
@@ -9,9 +9,9 @@ public class MyBase extends Base {
 				night();
 			}
 		}
-
+		
 		public void day() {
-			System.out.println("낮");
+			System.out.println("낮에는 열심히 일하자!");
 		}
 
 		public void night() {
@@ -19,7 +19,7 @@ public class MyBase extends Base {
 		}
 		
 		public void afternoon() {
-			System.out.println("오후");
+			System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
 		}
 	}
 
