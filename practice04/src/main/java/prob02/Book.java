50369ce90e3e417534cba5563f33292486201cc5
STRnick <nicksumin97@naver.com>
2022-04-28 22:25:24 +0900

    Prob02 Solved

--- practice04/src/main/java/prob02/Book.java
+++ practice04/src/main/java/prob02/Book.java
@@ -2,7 +2,7 @@ package prob02;
 
 public class Book {
 	private int bookNo;
-	private String title, author;
+	private String title, author, state;
 	private int stateCode = 1; // 0: 대여중, 1: 재고 있음
 
 	public int getBookNo() {
@@ -30,12 +30,24 @@ public class Book {
 	}
 
 	public Book(int bookNo, String title, String author) {
-		
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+	}
+
+	public void ck_rent() {
+		stateCode = 0;
+		System.out.println(title + "(가) 대여 됐습니다.");
 	}
 
 	public void print() {
-		// TODO Auto-generated method stub
-		
+		if (stateCode == 0) {
+			state = "대여중";
+		} else {
+			state = "재고있음";
+		}
+		System.out.println("책 제목:" + title + ", 작가:" + author + ", 대여 유무:" + state);
+
 	}
 
 }
