a889987e86fe98397da022727ac2fd8c6dc5421e
STRnick <nicksumin97@naver.com>
2022-04-28 22:06:11 +0900

    Prob01 Resolved

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,60 +1,20 @@
 package prob01;
 
 public class Printer {
-	private int num;
-	private boolean a;
-	private double b;
-	private String name;
-
-	public int getNum() {
-		return num;
-	}
-
-	public void setNum(int num) {
-		this.num = num;
-	}
-
-	public boolean isA() {
-		return a;
-	}
-
-	public void setA(boolean a) {
-		this.a = a;
-	}
-
-	public double getB() {
-		return b;
-	}
-
-	public void setB(double b) {
-		this.b = b;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public void println(int num) {
-		this.num = num;
 		System.out.println(num);
 	}
 
 	public void println(double b) {
-		this.b = b;
 		System.out.println(b);
 	}
 
 	public void println(boolean a) {
-		this.a = a;
 		System.out.println(a);
 	}
 
 	public void println(String name) {
-		this.name = name;
 		System.out.println(name);
 	}
 }
