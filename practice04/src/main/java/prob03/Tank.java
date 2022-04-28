64b0237c3130affe4631fd6d4fb81855f3c6ef86
STRnick <nicksumin97@naver.com>
2022-04-28 17:45:55 +0900

    Prob03 Solved

--- practice04/src/main/java/prob03/Tank.java
+++ practice04/src/main/java/prob03/Tank.java
@@ -4,11 +4,13 @@ public class Tank extends Unit{
 	// 현재 위치
 	private int x;
 	private int y;
-
+	
+	@Override
 	void move(int x, int y) {
 		/* 지정된 위치로 이동 */
 	}
-
+	
+	@Override
 	void stop() {
 		/* 현재 위치에 정지 */
 	}
