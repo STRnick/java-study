3c707b952d0a6ec305b8adfa31aee03fd81bd9fe
STRnick <nicksumin97@naver.com>
2022-04-29 12:16:47 +0900

    Exception #finish

---
+++ chapter03/src/main/java/exception/MyClass.java
@@ -0,0 +1,17 @@
+package exception;
+
+import java.io.IOException;
+
+public class MyClass {
+	public void danger() throws IOException, MyException {
+		System.out.println("some code...1");
+		System.out.println("some code...2");
+		
+		if(10 - 10 == 0) {
+			throw new MyException();
+		}
+		
+		System.out.println("some code...3");
+		System.out.println("some code...4");
+	}
+}
