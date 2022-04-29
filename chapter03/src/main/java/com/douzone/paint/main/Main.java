c076206cd12011a8dfbf6533d76d5cc3db8f33fc
STRnick <nicksumin97@naver.com>
2022-04-29 10:42:22 +0900

    Paint Re-Packaging

--- chapter03/src/main/java/com/douzone/paint/main/Main.java
+++ chapter03/src/main/java/com/douzone/paint/main/Main.java
@@ -1,54 +1,63 @@
-package paint;
+package com.douzone.paint.main;
 
-import javax.swing.text.GapContent;
+import com.douzone.paint.i.Drawable;
+import com.douzone.paint.point.ColorPoint;
+import com.douzone.paint.point.Point;
+import com.douzone.paint.shape.Circle;
+import com.douzone.paint.shape.Rectangle;
+import com.douzone.paint.shape.Shape;
+import com.douzone.paint.shape.Triangle;
+import com.douzone.paint.text.Graphictext;
 
 public class Main {
 
 	public static void main(String[] args) {
+
 //		Point point1 = new Point();
 //		point1.setX(20);
 //		point1.setY(10);
 //		point1.show();
+
 		Point point1 = new Point(20, 10);
 		drawPoint(point1);
-		
+
 		Point point2 = new Point(50, 100);
 		draw(point2);
 		point2.show(false);
-		
+
 		Point point3 = new ColorPoint(40, 50, "red");
 		draw(point3);
 //		point3.show(true);
-		
+
 		Triangle triangle = new Triangle();
 		draw(triangle);
-		
+
 		Rectangle rectangle = new Rectangle();
-		draw(rectangle);		
-		
+		draw(rectangle);
+
 		Circle circle = new Circle();
 		draw(circle);
-		
+
 		Graphictext graphictext = new Graphictext("Hello world!");
 		draw(graphictext);
-		
+
 		// instanceof 연산자 테스트
 		System.out.println(circle instanceof Object);
 		System.out.println(circle instanceof Shape);
 		System.out.println(circle instanceof Circle);
-		
+
 		// 오류: class는 Hierachy 상위와 하위만 instanceof 연산자를 사용할 수 있다.
 //		System.out.println(circle instanceof Rectangle);
-		
+
 		// interface는 Hierachy와 상관없이 instanceof 연산자를 사용할 수 있다.
 		System.out.println(circle instanceof Drawable);
 		System.out.println(circle instanceof Runnable);
 	}
-	
+
 	public static void draw(Drawable drawable) {
 		drawable.draw();
 	}
-	
+
 	public static void drawPoint(Point point) {
 		point.show();
 	}
@@ -56,11 +65,11 @@ public class Main {
 //	public static void drawShape(Shape shape) {
 //		shape.draw();
 //	}
-	
+
 //	public static void drawColorPoint(ColorPoint point) {
 //		point.show();
 //	}
-	
+
 //	public static void drawTriangle(Triangle triangle) {
 //		triangle.draw();
 //	}
