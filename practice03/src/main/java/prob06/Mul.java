b4002b027d45f528c17162c499e2f6c869cee25e
STRnick <nicksumin97@naver.com>
2022-05-02 00:07:35 +0900

    Prob06 modify

--- practice03/src/main/java/prob06/Mul.java
+++ practice03/src/main/java/prob06/Mul.java
@@ -3,11 +3,12 @@ package prob06;
 public class Mul {
 	private int a;
 	private int b;
+
 	public void setValue(int a, int b) {
 		this.a = a;
 		this.b = b;
 	}
-	
+
 	public int calculate() {
 		return a * b;
 	}
--- practice03/src/main/java/prob06/Sub.java
+++ practice03/src/main/java/prob06/Sub.java
@@ -3,11 +3,12 @@ package prob06;
 public class Sub {
 	private int a;
 	private int b;
+
 	public void setValue(int a, int b) {
 		this.a = a;
 		this.b = b;
 	}
-	
+
 	public int calculate() {
 		return a - b;
 	}
