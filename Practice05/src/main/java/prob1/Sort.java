a50b77ce721f0c683d732af51b4f400b35fb22d5
STRnick <nicksumin97@naver.com>
2022-05-01 23:22:31 +0900

    Prob1 Solved

--- Practice05/src/main/java/prob1/Sort.java
+++ Practice05/src/main/java/prob1/Sort.java
@@ -16,7 +16,16 @@ public class Sort {
 		//
 		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
 		//
-
+		
+		for(int i = 0; i < count; i++) {
+			for(int j = 0; j < count-i-1; j++) {
+				if(array[j] > array[j+1]) {
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
+			}
+		}
 		
 		// 결과 출력
 		System.out.println( "\nAfter Sort." );
