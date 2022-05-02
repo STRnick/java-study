f85cd593622730d604f95310448c37d7c7e0afb1
STRnick <nicksumin97@naver.com>
2022-05-02 23:44:09 +0900

    Prob5 Solved

--- Practice05/src/main/java/prob5/MyStack.java
+++ Practice05/src/main/java/prob5/MyStack.java
@@ -3,16 +3,35 @@ package prob5;
 public class MyStack {
 	private int top = 0;
 	private String[] buffer;
-
-	public MyStack(int size ) {
-		buffer = new String[size],
+	
+	public MyStack(int size) {
+		buffer = new String[size];
 	}
 
-	public String pop() {
-		return null;
+	private void resize(){
+		if(top + 1 == buffer.length){
+			String[] newBuffer = new String[buffer.length * 2];
+			System.arraycopy(buffer, 0, newBuffer, 0, top+1);
+			buffer = newBuffer;
+		}
 	}
 
-	public boolean isEmpty() {
+	public void push(String item) {
+		resize();
+		buffer[++top] = item;		
+	}
 
+	public String pop() throws MyStackException{
+		if(top == 0){
+			throw new MyStackException();
+		}
+		String item = buffer[top];
+		buffer[top] = null;
+		top--;
+		return item;
+	}
+	
+	public boolean isEmpty() {		
+		return (top == 0);
 	}
 }
