79ee7b612ba5fcf2750710d6a016e1de55537e8d
STRnick <nicksumin97@naver.com>
2022-05-02 23:45:39 +0900

    chapter03 ArrayUtil modify

--- chapter03/src/main/java/chapter03/ArrayUtil.java
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -1,29 +1,35 @@
 package chapter03;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 public class ArrayUtil {
-
 	public static double[] intToDouble(int[] a) {
-		double[] result = new double[a.length];
-		for(int i=0;i<a.length;i++) {
-			result[i] = a[i];
+		double[] d = new double[a.length];
+
+		for (int i = 0; i < d.length; i++) {
+			d[i] = a[i];
 		}
-		
-		return result;
+		return d;
 	}
 
-	public static int[] doubleToInt(double[] ds) {
-		int[] result_int = new int[ds.length];
-		for(int j=0;j<ds.length;j++) {
-			result_int[j] = (int) ds[j];
+	public static int[] doubleToInt(double[] d) {
+		int[] a = new int[d.length];
+
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (int) d[i];
 		}
-		return result_int;
+		return a;
 	}
 
-	public static int[] concat(int[] is1, int[] is2) {
-		
-		return null;
+	public static int[] concat(int[] a1, int[] a2) {
+		int[] a = new int[a1.length + a2.length];
+		int index = 0;
+
+		for (int i : a1) {
+			a[index++] = i;
+		}
+
+		for (int i : a2) {
+			a[index++] = i;
+		}
+		return a;
 	}
 }
