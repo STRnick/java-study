02de41d069b3e392059fb5548bac32555b9c5469
STRnick <nicksumin97@naver.com>
2022-05-02 12:02:03 +0900

    calendar #finish

--- chapter04/src/main/java/chapter04/MyClassTest.java
+++ chapter04/src/main/java/chapter04/MyClassTest.java
@@ -4,5 +4,11 @@ public class MyClassTest {
 
 	public static void main(String[] args) {
 		MyClass cal = MyClass.getInstance();
+		
+		printDate(cal);
+	}
+
+	private static void printDate(MyClass cal) {
+		
 	}
 }
